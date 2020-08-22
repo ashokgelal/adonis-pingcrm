723fcbacd5d28b16578403b4fa9ef5dec8e2beae
Ashok Gelal <>
2020-08-22 10:25:23 -0600

    Add authentication

---
+++ app/Controllers/Http/Auth/LoginController.ts
@@ -0,0 +1,13 @@
+import {HttpContextContract} from "@ioc:Adonis/Core/HttpContext";
+
+export default class LoginController {
+  public async showLoginForm({inertia}: HttpContextContract) {
+    await inertia.render("Auth/Login")
+  }
+
+  public async login({request, auth, response}: HttpContextContract) {
+    const {email, password} = request.all()
+    await auth.attempt(email, password)
+    return response.redirect("/")
+  }
+}
